b536bc640268a16c3db8d42a8f98098712852760
bklassen5-rrc <bklassen5@rrc.ca>
2026-01-12 20:47:29 -0600

    Updated app.tsx to work with new file structure.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import Landing from "./components/pages/Landing";
-import Login from "./components/pages/Login";
+import Login from "./components/pages/Login/Login";
 
 function App() {
   return (
