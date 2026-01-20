ca9e4a454defe41073a229c3679079f938d37e99
rhajiserdar <rhajiserdar@academic.rrc.ca>
2026-01-19 19:19:19 -0600

    Created AllyIcon.tsx and Battlescreen.tsx files.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import Landing from "./components/pages/Landing";
 import Login from "./components/pages/Login";
+import BattleScreen from "./components/pages/battlescreen/Battlescreen";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/battle" element={<BattleScreen />} />
         </Routes>
       </main>
 
