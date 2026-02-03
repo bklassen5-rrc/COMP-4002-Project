11be77450c162d8818eb1bc6b702455869e8cf2a
ladamo-rrc <ladamo@rrc.ca>
2026-02-02 19:00:35 -0600

    Added basic functionality for discard screen.

--- src/assets/components/pages/battleComplete.tsx
+++ src/assets/components/pages/battleComplete.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import './battleComplete.css'
 
 function BattleComplete() {
     // placeholder data
 
     const experienceGained = 150
-    const itemsDropped = ["Minor Health Potion", "Broken Straight Sword"]
+    // const itemsDropped = ["Minor Health Potion", "Broken Straight Sword"]
+    const [itemsDropped, setItemsDropped] = useState([
+        {id: 1, value: "Minor Health Potion"},
+        {id: 2, value: "Broken Straight Sword"},
+        {id: 3, value: "A rusted nail"},
+        {id: 4, value: "An all-seeing eyeball"}
+    ])
+    const [itemsDiscarded, setItemsDiscarded] = useState<{id: number, value: string}[]>([])
     return(
         <div className="postBattleOverlay">
             <div className="postBattleModal">
@@ -13,10 +21,35 @@ function BattleComplete() {
                 <p>Experience gained: {experienceGained}</p>
                 <div>
                     <h3>Items Dropped</h3>
-                    <ul>
-                        {itemsDropped.map((item) =>(
-                        <li key={item}>{item}</li>))}
+                    <p>Select the ones you'd like to discard.</p>
+                   <ul>
+                        {
+                            itemsDropped.map((items) => {
+                                return(
+                                    <li key={items.id}
+                                        onClick={() => {
+                                            setItemsDropped(oldList => oldList.filter(filteredItems => filteredItems.id != items.id));
+                                            setItemsDiscarded(oldItems => [...oldItems, items])
+                                        }}
+                                    >
+                                        {items.value}
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
+
+                    <h3>Are you sure you want to discard these items?</h3>
+                    <ul>
+                        {itemsDiscarded.map((itemsDiscard) => {
+                            return(
+                                <li key = {itemsDiscard.id}>
+                                    {itemsDiscard.value}
+                                </li>
+                            );
+                        })}
+                    </ul>
+
                     <p className='baggedGoods'>You safely tuck the items into your bag.</p>
                     <p className='baggedGoods'>Best be on your way.</p>
                     <button className="continueButton">continue</button>
