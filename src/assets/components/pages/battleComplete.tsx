2dc30b2bb840d774bd22ff19634c3c991eff7d45
ladamo-rrc <ladamo@rrc.ca>
2026-02-02 19:49:57 -0600

    discard feature is working.

--- src/assets/components/pages/battleComplete.tsx
+++ src/assets/components/pages/battleComplete.tsx
@@ -13,17 +13,17 @@ function BattleComplete() {
     // placeholder data
 
     const experienceGained = 150
-    // const itemsDropped = ["Minor Health Potion", "Broken Straight Sword"]
     const [itemsDropped, setItemsDropped] = useState([
         {id: 1, value: "Minor Health Potion"},
         {id: 2, value: "Broken Straight Sword"},
         {id: 3, value: "A rusted nail"},
         {id: 4, value: "An all-seeing eyeball"}
     ])
-    // items that are 'dropped' after the fight
+    
     const [itemsDiscarded, setItemsDiscarded] = useState<{id: number, value: string}[]>([])
-    // need another state to track items that player doesn't want anymore
     const[selectedItems, setSelectedItems] = useState<{id: number, value: string}[]>([])
+    const[discardConfirmation, setDiscardConfirmation] = useState(false)
+    const[itemsKept, setItemsKept] = useState(false)
     
     return(
         <div className="postBattleOverlay">
@@ -33,7 +33,6 @@ function BattleComplete() {
                 <p>Experience gained: {experienceGained}</p>
                 <div>
                     <h3>Items Dropped</h3>
-                    <p>Select the ones you'd like to discard:</p>
                     <br></br>
                    <ul>
                         {
@@ -56,20 +55,52 @@ function BattleComplete() {
                             })
                         }
                     </ul>
+                    <p>Select the ones you'd like to discard.</p>
+                    <br></br>
+                    <button onClick={() =>{
+                        setDiscardConfirmation(true)
+                    }}>
+                        Discard Items
+                    </button>
+                    {discardConfirmation && (
+                        <div>
+                            <h3>Are you sure you want to discard these items?</h3>
+                            <ul>
+                                {selectedItems.map((itemsDiscard) => {
+                                    return(
+                                        <li key = {itemsDiscard.id}>
+                                            {itemsDiscard.value}
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                                <button onClick={() => {
+                                    setDiscardConfirmation(false)
+                                }}>
+                                    Cancel
+                                </button>
+                                <button onClick={() => {
+                                    setItemsDiscarded(oldItems => [...oldItems, ...selectedItems])
+                                    setItemsDropped(oldList =>
+                                        oldList.filter(item =>
+                                            selectedItems.filter(selectedItem => selectedItem.id === item.id).length===0
+                                        )
+                                    )
+                                    setSelectedItems([])
+                                    setItemsKept(true)
+                                    setDiscardConfirmation(false)
+                                }}>
+                                    Confirm
+                                </button>
+                    </div>
+                    )}
 
-                    <h3>Are you sure you want to discard these items?</h3>
-                    <ul>
-                        {itemsDiscarded.map((itemsDiscard) => {
-                            return(
-                                <li key = {itemsDiscard.id}>
-                                    {itemsDiscard.value}
-                                </li>
-                            );
-                        })}
-                    </ul>
-
-                    <p className='baggedGoods'>You safely tuck the items into your bag.</p>
-                    <p className='baggedGoods'>Best be on your way.</p>
+                    {itemsKept &&(
+                        <div>
+                            <p className='baggedGoods'>You drop the items and safely tuck the kept ones into your bag.</p>
+                            <p className='baggedGoods'>Best be on your way.</p>
+                        </div>
+                    )}
                     <button className="continueButton">continue</button>
                 </div>
             </div>
